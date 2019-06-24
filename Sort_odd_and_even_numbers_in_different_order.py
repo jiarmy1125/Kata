e24e00dccfb3fd4b469d448cca37ee7e626142fc
D0448495 <jiarmy1125@gmail.com>
2019-06-24 16:24:43 +0800

    Kata01

--- Sort_odd_and_even_numbers_in_different_order.py
+++ Sort_odd_and_even_numbers_in_different_order.py
@@ -4,32 +4,32 @@
 # sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 8, 4, 5, 2]
 
 def sort_array(a):
-    x = []
-    y = []
-    z = []
+    odd = []
+    even = []
+    new = []
 
-    for i in a:
+    for i in a: #分奇數偶數
         if i%2==0:
-            x.append(i)
+            odd.append(i)
 
         else:
-            y.append(i)
+            even.append(i)
 
-    b = sorted(x,reverse=True)
-    c = sorted(y)
+    odd_reverse = sorted(odd,reverse=True) #將偶數降冪排列
+    even_sort = sorted(even)   #將奇數升冪排列
 
     j=0
     k=0
-    for i in a:
+    for i in a: #將數字重新放回原陣列中
         if i%2==0:
-            z.append(b[j])
+            new.append(odd_reverse[j])
             j += 1
             
         else: 
-            z.append(c[k])
+            new.append(even_sort[k])
             k += 1
 
-    return z
+    return new
 
 result = sort_array([5, 3, 2, 8, 1, 4])
 print(result)
