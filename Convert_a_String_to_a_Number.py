a3212d823afb21a99790382093d855edb6203e1d
D0448495 <jiarmy1125@gmail.com>
2019-07-05 15:39:15 +0800

    0705

--- Convert_a_String_to_a_Number.py
+++ Convert_a_String_to_a_Number.py
@@ -3,11 +3,9 @@
 # stringToNumber("1405") == 1405
 # stringToNumber("-7"  ) == -7
 
-def stringToNumber(s):
+def string_to_number(s):
     s = int(s)
     print(s)
+    return s
 
-
-
-stringToNumber("1234")
-print(type("1234"))
+string_to_number("1234")
