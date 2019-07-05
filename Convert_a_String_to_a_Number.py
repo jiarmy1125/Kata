4f97d2b514620e8af5a7753b9828a1f7f9cc5e8d
D0448495 <jiarmy1125@gmail.com>
2019-07-05 15:29:49 +0800

    0705

---
+++ Convert_a_String_to_a_Number.py
@@ -0,0 +1,13 @@
+# stringToNumber("1234") == 1234
+# stringToNumber("605" ) == 605
+# stringToNumber("1405") == 1405
+# stringToNumber("-7"  ) == -7
+
+def stringToNumber(s):
+    s = int(s)
+    print(s)
+
+
+
+stringToNumber("1234")
+print(type("1234"))
