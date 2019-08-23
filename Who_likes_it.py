857205a63f405c9245dac0577a3a85cd96916bf3
D0448495 <jiarmy1125@gmail.com>
2019-08-23 15:37:10 +0800

    0823

--- Who_likes_it.py
+++ Who_likes_it.py
@@ -9,7 +9,7 @@ def likes(names):
         return ('no one likes this')
 
     elif len(names) == 1:
-        return (names[0] + ' like this')
+        return (names[0] + ' likes this')
 
     elif len(names) == 2:
         return (names[0] + ' and ' + names[1] + ' like this')
