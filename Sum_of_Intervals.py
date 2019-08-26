e0afb343da86f31c251e36b26dc097682e9f2018
D0448495 <jiarmy1125@gmail.com>
2019-08-26 16:38:26 +0800

    0826

---
+++ Sum_of_Intervals.py
@@ -0,0 +1,28 @@
+# sumIntervals( [[1,2],[6, 10],[11, 15]] ); // => 9
+# sumIntervals( [[1,4],[7, 10],[3, 5]] ); // => 7
+# sum_of_intervals([(1, 5)]), 4
+# sum_of_intervals([(1, 5), (6, 10)]), 8
+# sum_of_intervals([(1, 5), (1, 5)]), 4
+# sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7
+
+def sum_of_intervals(intervals):
+    
+    x=[]
+
+    for tu in intervals:
+        for i in range(tu[0],tu[1]):
+            x.append(i)
+    
+    print(x)
+
+    x=set(x)    
+    print(x)
+    total=len(x)
+    print(total)
+    return total
+
+sum_of_intervals([(1, 5)]) ,4
+sum_of_intervals([(1, 5), (6, 10)]), 8    
+sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7
+
+
